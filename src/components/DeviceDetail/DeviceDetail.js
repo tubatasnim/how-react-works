486473ce768fedb4a047b2580c71804ad0254b5b
tubatasnim <tasnimtuba22@gmail.com>
2022-03-24 10:16:27 +0600

    first commit

---
+++ src/components/DeviceDetail/DeviceDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeviceDetail = (props) => {
+    return (
+        <div>
+            <h4>Price:{props.price}</h4>
+        </div>
+    );
+};
+
+export default DeviceDetail;
